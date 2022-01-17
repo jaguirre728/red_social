8f5497b362a7c923a7f1e328783f31a160ed3db5
Jorge Aguirre <sjear728@gmail.com>
2022-01-17 01:18:18 -0500

    se crea conexión a la bd

---
+++ bd/conexionBD.go
@@ -0,0 +1,38 @@
+package bd
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+/*MongoCN es el objeto de conexión a la bd*/
+var MongoCN = ConectarBD()
+var clientOptions = options.Client().ApplyURI("mongodb+srv://prueba1:jear018@cluster0.9ezvx.mongodb.net/test")
+
+/*ConectarBD es la función que me permite conectar a la bd */
+func ConectarBD() *mongo.Client {
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	log.Println("Conexión exitosa con la BD")
+	return client
+}
+
+/*ChequeoConnection es el ping a la bd*/
+func ChequeoConnection() int {
+	err := MongoCN.Ping(context.TODO(), nil)
+	if err != nil {
+		return 0
+	}
+	return 1
+}
--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/jaguirre728/red_social/bd"
+	"github.com/jaguirre728/red_social/handlers"
+)
+
+func main() {
+	if bd.ChequeoConnection() == 0 {
+		log.Fatal("Sin conexión a la base de datos")
+		return
+	}
+	handlers.Manejadores()
 }
