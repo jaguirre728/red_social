4fe5e139d7da0d5462a2fd1ea043dad1378216a8
jaguirre728 <sjear728@gmail.com>
2022-01-30 17:25:59 -0500

    verperfil

---
+++ bd/buscoPerfil.go
@@ -0,0 +1,36 @@
+package bd
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/jaguirre728/red_social.git/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+/*BuscoPerfil busca un perfil en la base de datos*/
+func BuscoPerfil(ID string) (models.Usuario, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+
+	db := MongoCN.Database("red_social")
+	col := db.Collection("usuarios")
+
+	var perfil models.Usuario
+	objID, _ := primitive.ObjectIDFromHex(ID)
+
+	condicion := bson.M{
+		"_id": objID,
+	}
+
+	err := col.FindOne(ctx, condicion).Decode(&perfil)
+	perfil.Password = ""
+	if err != nil {
+		fmt.Println("Registro no encontrado" + err.Error())
+		return perfil, err
+	}
+	return perfil, nil
+
+}
