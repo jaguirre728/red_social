9cacac171f2338f82b673f2d407e3f65d38b82c8
jaguirre728 <sjear728@gmail.com>
2022-01-30 22:58:36 -0500

    se agrega modificar perfil

---
+++ routers/modificarPerfil.go
@@ -0,0 +1,35 @@
+package routers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/jaguirre728/red_social.git/bd"
+	"github.com/jaguirre728/red_social.git/models"
+)
+
+/*ModificarPerfil modifica el perfil de un usuario*/
+func ModificarPerfil(w http.ResponseWriter, r *http.Request) {
+	var t models.Usuario
+
+	err := json.NewDecoder(r.Body).Decode(&t)
+	if err != nil {
+		http.Error(w, "Datos Incorrectos"+err.Error(), 400)
+		return
+	}
+
+	var status bool
+	status, err = bd.ModificoRegistro(t, IDUsuario)
+	if err != nil {
+		http.Error(w, "Ocurrió un error al intentar modificar el registro, Reintente nuevamente"+err.Error(), 400)
+		return
+	}
+
+	if status == false {
+		http.Error(w, "No se ha logrado modificar el registro del usuario", 400)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+
+}
